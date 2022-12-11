18290b0f79fc7b1cf5a5c6406876d13da1da85df
ohcristo <cricrigrassi@gmail.com>
2022-12-11 17:14:14 +0000

    test node

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',nome:'Cristiano' });
+});
+
+router.get('/test', function(req, res, next) {
+  res.render('test', { title: 'Express',nome:'Cristiano' , cibo:'cioccolato'});
 });
 
 module.exports = router;
