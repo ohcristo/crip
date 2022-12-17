03661461036918b4005ab90a71c43cf467d721d0
ohcristo <cricrigrassi@gmail.com>
2022-12-17 23:57:03 +0000

    node test

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const users = require('../user.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Centro',nome:'Cristiano' });
 });
 
+router.get('/sign-in', function(req, res, next) {
+  res.render('signin', { title: 'sign-in',nome:users.nome });
+});
+
+router.post('/sign-in', function(req, res, next) {
+  let utero=req.body.uter;
+  let passo=req.body.passuter;
+  users[utero]["pass"]=passo;
+  res.render('save',{utero:utero,passo:passo,title:'Salvataggio'});
+});
 
 
 
--- routes/inter.js
+++ routes/inter.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const multipart = require('connect-multiparty');
+const multipartMiddleware = multipart({uploadDir: './uploads'});
+
 router.get('/', function(req, res, next) {
     res.render('interazione', { title: 'Interazione',nome:'Cristiano' });
   });
@@ -9,5 +12,13 @@ router.post('/test', function(req, res, next) {
   let nomeutente = req.body.Username
   res.render('test', { title: 'test',nome:'Cristiano' , cibo:'cioccolato',utente: nomeutente});
 }); 
+
+router.get('/inport', function(req, res, next) {
+  res.render('inport', { title: 'Inport',nome:'Cristiano' });
+});
+
+router.post('/inport',multipartMiddleware, function(req, res, next) {
+  res.send('<h2>File caricato</h2>')
+});
 
 module.exports = router;
